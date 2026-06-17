96461a8e8278b2f17c4b787b9ab1f407796fc59d
skdharan007-coder <skdharan007@gmail.com>
2026-06-17 16:15:09 +0530

    Add files via upload

---
+++ src/components/Products_set.jsx
@@ -0,0 +1,45 @@
+import prod1 from "../assets/prod1.jpg"
+import prod2 from "../assets/prod2.jpg"
+import prod3  from "../assets/prod3.jpg"
+import prod4 from"../assets/prod4.jpg";
+
+const products_set = [
+  {
+    id: 1,
+    name: "Linen Blend Shirt In Black",
+    price:2000,
+    image: prod1
+    
+  },
+
+  {
+    id:2,
+    name:"Melange Shirt Solid Beige",
+    price:1500,
+    image:prod2
+  },
+
+  {
+    id:3,
+    name:"Brown Shirt With A Classic",
+    price:1000,
+    image:prod3
+  },
+
+  {
+    id:4,
+    name:"White All Over Print Slim",
+    price:2500,
+    image:prod4
+  },
+
+  {
+    id:5,
+    name:"White All Over Print Slim",
+    price:2500,
+    image:prod4
+  }
+  
+];
+
+export default products_set;
